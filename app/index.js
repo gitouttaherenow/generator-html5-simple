f3532fadd17138dd604662b067d9f3a8049683d2
Thoshi <thoshikesh@gmail.com>
2018-07-20 20:40:53 +0530

    "added reset.css and basic scaffolding"

--- app/index.js
+++ app/index.js
@@ -1,11 +1,30 @@
 'use strict';
 const Generator = require('yeoman-generator');
+
 module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts);
     this.log('Initializing...');
   }
   start() {
-    this.log('Do something...');
+    this.prompt([
+        {
+          type    : 'input',
+          name    : 'name',
+          message : 'Enter a name for your project '
+        }
+	]).then( (answers) => {
+	    // create destination folder
+	    //this.destinationRoot(answers.name);
+
+	    this.fs.copyTpl(
+	      this.templatePath('index.html'),
+	      this.destinationPath('index.html'),
+	      { title: answers.name }
+	    );
+	    this.fs.copy(this.templatePath('css/style.css'), this.destinationPath('css/style.css'));
+	    this.fs.copy(this.templatePath('css/reset.css'), this.destinationPath('css/reset.css'));
+	    this.fs.copy(this.templatePath('js/scripts.js'), this.destinationPath('js/scripts.js'));
+	});
   }
 };
